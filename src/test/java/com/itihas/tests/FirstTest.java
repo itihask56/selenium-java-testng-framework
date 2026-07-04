80d6c93146b7cb93152c9f8f9c165cbf709b7944
Itihas Verma <dev.itihas@gmail.com>
2026-07-04 15:12:58 +0530

    feat: complete lesson 10 first Selenium automation

---
+++ src/test/java/com/itihas/tests/FirstTest.java
@@ -0,0 +1,60 @@
+package com.itihas.tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class FirstTest {
+
+    public static void main(String[] args) throws InterruptedException {
+        ChromeOptions options = new ChromeOptions();
+
+        Map<String, Object> prefs = new HashMap<>();
+
+        prefs.put("credentials_enable_service", false);
+        prefs.put("profile.password_manager_enabled", false);
+        prefs.put("profile.password_manager_leak_detection", false);
+
+        options.setExperimentalOption("prefs", prefs);
+
+        options.addArguments("--disable-save-password-bubble");
+        options.addArguments("--disable-features=PasswordLeakDetection");
+        options.addArguments("--disable-features=PasswordManagerOnboarding");
+
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.get("https://www.saucedemo.com/");
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+        String title = driver.getTitle();
+
+        if(title.equals("Swag Labs")){
+            System.out.println("Title: "+title);
+
+        }
+        else {
+            System.out.println("Title Not Found");
+        }
+
+        driver.findElement(By.cssSelector("img[alt='Sauce Labs Bolt T-Shirt']")).click();
+
+
+        driver.findElement(By.id("add-to-cart")).click();
+        driver.findElement(By.className("shopping_cart_link")).click();
+        driver.findElement(By.id("checkout")).click();
+        driver.findElement(By.id("first-name")).sendKeys("Itihas");
+        driver.findElement(By.id("last-name")).sendKeys("Verma");
+        driver.findElement(By.id("postal-code")).sendKeys("805127");
+        driver.findElement(By.id("continue")).click();
+        driver.findElement(By.id("finish")).click();
+        Thread.sleep(3000);
+        driver.quit();
+    }
+}
