1c2a2604cb36315bf4193f9b96b54cecd20c6171
Itihas Verma <dev.itihas@gmail.com>
2026-07-11 19:37:08 +0530

    feat: implement Page Object Model with BasePage and BaseTest

---
+++ src/test/java/com/itihas/base/BaseTest.java
@@ -0,0 +1,23 @@
+package com.itihas.base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseTest {
+
+    protected WebDriver driver;
+
+    public void setup(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.saucedemo.com/");
+    }
+
+    public void tearDown(){
+
+        if(driver!=null){
+            driver.quit();
+        }
+
+    }
+}
--- src/test/java/com/itihas/tests/LoginTest.java
+++ src/test/java/com/itihas/tests/LoginTest.java
@@ -1,30 +1,31 @@
 package com.itihas.tests;
 
+import com.itihas.base.BaseTest;
 import com.itihas.pages.LoginPage;
 import com.itihas.pages.ProductsPage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class LoginTest {
+public class LoginTest extends BaseTest {
 
     public static void main(String[] args) {
 
-        WebDriver driver = new ChromeDriver();
+         LoginTest test = new LoginTest();
+         test.setup();
 
-        driver.get("https://www.saucedemo.com/");
-
-        LoginPage loginPage = new LoginPage(driver);
+        LoginPage loginPage = new LoginPage(test.driver);
 
         loginPage.login(
                 "standard_user",
                 "secret_sauce"
         );
 
-        ProductsPage productsPage = new ProductsPage(driver);
+        ProductsPage productsPage = new ProductsPage(test.driver);
         System.out.println(productsPage.getTotalProducts());
         productsPage.printAllProducts();
         productsPage.addProductsBelowPrice(20);
 
-        driver.quit();
+//        driver.quit();
+        test.tearDown();
     }
 }
